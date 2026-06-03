30358a1d8449e1b2aef6f086d6ea03fb6b582cf4
Anujr_shri <shrivastavaanuj059@gmail.com>
2026-06-03 11:58:51 +0530

    Update chat_with_llm.py

--- App/pages/chat_with_llm.py
+++ App/pages/chat_with_llm.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_icon="👋",
 )
 
-base_url = "http://127.0.0.1:8000"
+base_url = " https://gen-ai1.onrender.com"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
